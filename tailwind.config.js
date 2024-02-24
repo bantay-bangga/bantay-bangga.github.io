32fd9495620eefa656cb141ead5650ac5201f04a
Basti Ortiz <39114273+BastiDood@users.noreply.github.com>
2024-02-24 14:15:01 +0800

    chore: initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,7 @@
+import typo from '@tailwindcss/typography';
+
+/** @type {import('tailwindcss').Config} */
+export default {
+    content: ['./src/**/*.{css,html,js,svelte,ts}'],
+    plugins: [typo],
+};
