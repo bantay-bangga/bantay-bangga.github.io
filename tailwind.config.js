39e3263500a2c821d49586e459e4d2e631edaa10
Basti Ortiz <39114273+BastiDood@users.noreply.github.com>
2024-02-26 01:03:19 +0800

    fix(tw): apply global variable optimizations

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,7 @@ import typo from '@tailwindcss/typography';
 
 /** @type {import('tailwindcss').Config} */
 export default {
+    experimental: { optimizeUniversalDefaults: true },
     content: ['./src/**/*.{css,html,js,svelte,ts}'],
     plugins: [typo],
 };
