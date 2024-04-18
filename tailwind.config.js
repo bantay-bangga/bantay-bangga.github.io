7c3aa2cb408cf567662b1dadc380db9e6e8cd368
Anjellyrika <98273014+Anjellyrika@users.noreply.github.com>
2024-04-18 19:13:11 +0800

    feat(tw): add basic color palette

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,4 +5,17 @@ export default {
     experimental: { optimizeUniversalDefaults: true },
     content: ['./src/**/*.{css,html,js,svelte,ts}'],
     plugins: [typo],
+    theme: {
+        extend: {
+            colors: {
+                'primary': '#F9AE22',
+                'secondary': '#DB6659',
+                'tertiary': {
+                    50: '#EBF0F0',
+                    400: '#7C999B',
+                },
+                'surface': '#272A2C',
+            }
+        }
+    }
 };
