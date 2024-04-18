8a2db16d50365cb8b604ca11cf108e4a4997d4e4
Basti Ortiz <39114273+BastiDood@users.noreply.github.com>
2024-04-18 19:18:56 +0800

    chore: use TypeScript for Tailwind config

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,6 +1,6 @@
+import { Config } from 'tailwindcss';
 import typo from '@tailwindcss/typography';
 
-/** @type {import('tailwindcss').Config} */
 export default {
     experimental: { optimizeUniversalDefaults: true },
     content: ['./src/**/*.{css,html,js,svelte,ts}'],
@@ -8,14 +8,14 @@ export default {
     theme: {
         extend: {
             colors: {
-                'primary': '#F9AE22',
-                'secondary': '#DB6659',
-                'tertiary': {
+                primary: '#F9AE22',
+                secondary: '#DB6659',
+                tertiary: {
                     50: '#EBF0F0',
                     400: '#7C999B',
                 },
-                'surface': '#272A2C',
-            }
-        }
-    }
-};
+                surface: '#272A2C',
+            },
+        },
+    },
+} satisfies Config;
