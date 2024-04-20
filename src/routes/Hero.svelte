2083ed937b22481284f1d39997a171b3865ac386
Anjellyrika <98273014+Anjellyrika@users.noreply.github.com>
2024-04-21 01:28:00 +0800

    feat: implement media queries for cards

--- src/routes/Hero.svelte
+++ src/routes/Hero.svelte
@@ -1,6 +1,6 @@
-<div class="grid grid-cols-5 gap-2 text-surface">
+<div class="grid gap-2 text-surface md:grid-cols-5">
     <div
-        class="col-span-4 space-y-4 rounded-lg bg-primary px-10 py-12 shadow-md transition-transform hover:scale-105 hover:shadow-2xl"
+        class="space-y-4 rounded-lg bg-primary px-10 py-12 shadow-md transition-transform hover:scale-105 hover:shadow-2xl md:col-span-4"
     >
         <h1 class="font-anon text-7xl font-bold tracking-wider">Bantay Bangga</h1>
         <p class="font-raleway">
@@ -16,7 +16,7 @@
         <h2 class="w-min text-center font-anon text-4xl font-bold tracking-wider">2016 to 2024</h2>
     </div>
     <div
-        class="col-span-3 space-y-2 rounded-lg bg-tertiary-50 px-10 py-12 shadow-md transition-transform hover:scale-105 hover:shadow-2xl"
+        class="space-y-2 rounded-lg bg-tertiary-50 px-10 py-12 shadow-md transition-transform hover:scale-105 hover:shadow-2xl md:col-span-3"
     >
         <span class="text-gray-500">Researchers</span>
         <h2 class="font-anon text-4xl font-bold tracking-wider">FerrisWheel&#129408;</h2>
@@ -27,7 +27,7 @@
         </ul>
     </div>
     <div
-        class="col-span-2 flex items-center justify-center rounded-lg bg-tertiary-400 px-10 py-12 shadow-md transition-transform hover:scale-105 hover:shadow-2xl"
+        class="flex items-center justify-center rounded-lg bg-tertiary-400 px-10 py-12 shadow-md transition-transform hover:scale-105 hover:shadow-2xl md:col-span-2"
     >
         <p class="w-min text-center font-raleway">
             Approximately <span class="font-anon text-6xl font-bold">46209</span> recorded road incidents in the Philippines
