adde78115f880b1472716a7019974c8c173c7701
Dox-Dev <83331542+Dox-Dev@users.noreply.github.com>
2024-04-21 17:28:15 +0800

    feat: add `Overview` content

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -3,3 +3,11 @@
 </script>
 
 <Hero />
+
+<h1 class="text-4xl font-extrabold dark:text-white"> Overview </h1>
+
+<p class="dark:text-white">
+    The Bantay Bangga project is a data science initiative undertaken in fulfillment of CS 132 during the second semester of the academic year 2023-2024 at the University of the Philippines - Diliman.
+    The project aims to analyze and assess the change in road safety caused by the modified procedure of the issuance of driver's licenses by the Land Transportation Office (LTO) in the Philippines this past January 2020.
+    The dataset to be used for this analysis contains approximately 46000 entries of road accidents that span from the years 2016 to 2024.
+</p>
