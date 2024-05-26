69c053ee675f8305141b295c8f839e4747161f90
Basti Ortiz <39114273+BastiDood@users.noreply.github.com>
2024-05-26 21:20:08 +0800

    chore: remove dark mode styles

--- src/routes/DataLink.svelte
+++ src/routes/DataLink.svelte
@@ -6,6 +6,6 @@
 <a
     {href}
     target="_blank"
-    class="inline-flex items-center gap-2 rounded-lg bg-green-700 px-5 py-2.5 align-middle text-sm font-medium text-white no-underline hover:bg-green-800 focus:outline-none focus:ring-4 focus:ring-green-300 dark:bg-green-600 dark:hover:bg-green-700 dark:focus:ring-green-800"
+    class="inline-flex items-center gap-2 rounded-lg bg-green-700 px-5 py-2.5 align-middle text-sm font-medium text-white no-underline hover:bg-green-800 focus:outline-none focus:ring-4 focus:ring-green-300"
     ><slot /></a
 >
