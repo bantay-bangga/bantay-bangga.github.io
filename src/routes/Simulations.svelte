b3e9e22a650bab926405a439dd960ad3f0def4dc
Dox-Dev <83331542+Dox-Dev@users.noreply.github.com>
2024-06-19 22:53:10 +0800

    fix(`Simulations`): separate text into different parts

--- src/routes/Simulations.svelte
+++ src/routes/Simulations.svelte
@@ -1,7 +1,7 @@
 <h3 class="not-prose">Simulations</h3>
 <h4 class="not-prose">Minor Injury Predictions</h4>
-<p>
-    Given a total of:
+<p>Given a total of:</p>
+<ul>
     <li>
         <math xmlns="http://www.w3.org/1998/Math/MathML"><mrow><mn>600</mn></mrow></math> occupied ICU beds with COVID-19
         patients
@@ -15,15 +15,16 @@
         <math xmlns="http://www.w3.org/1998/Math/MathML"><mrow><mn>42</mn><mi>%</mi></mrow></math> mechanical ventilation
         unit usage rate
     </li>
-
+</ul>
+<p>
     The number of predicted minor injuries given the model was<math xmlns="http://www.w3.org/1998/Math/MathML"
         ><mrow><mn>11</mn><mi>.</mi><mn>7770</mn></mrow></math
     >.
 </p>
 
 <h4 class="not-prose">Serious Injury Predictions</h4>
-<p>
-    Given a total of:
+<p>Given a total of:</p>
+<ul>
     <li>
         <math xmlns="http://www.w3.org/1998/Math/MathML"><mrow><mn>3500</mn></mrow></math> occupied ICU beds with non-COVID-19
         patients
@@ -39,15 +40,16 @@
     <li>
         <math xmlns="http://www.w3.org/1998/Math/MathML"><mrow><mn>9500000</mn></mrow></math> available surgical masks
     </li>
-
+</ul>
+<p>
     The number of predicted serious injuries given the model was<math xmlns="http://www.w3.org/1998/Math/MathML"
         ><mrow><mn>1</mn><mi>.</mi><mn>0188</mn></mrow></math
     >.
 </p>
 
 <h4 class="not-prose">Fatal Injury Predictions</h4>
-<p>
-    Given a total of:
+<p>Given a total of:</p>
+<ul>
     <li>
         <math xmlns="http://www.w3.org/1998/Math/MathML"><mrow><mn>50000</mn></mrow></math> occupied beds (inclusive of ICU
         and non-ICU beds)
@@ -61,7 +63,8 @@
         <math xmlns="http://www.w3.org/1998/Math/MathML"><mrow><mn>10000000</mn></mrow></math> available surgical masks
     </li>
     <li><math xmlns="http://www.w3.org/1998/Math/MathML"><mrow><mn>1100000</mn></mrow></math> available N95 masks</li>
-
+</ul>
+<p>
     The number of predicted serious injuries given the model was<math xmlns="http://www.w3.org/1998/Math/MathML"
         ><mrow><mn>0</mn><mi>.</mi><mn>6222</mn></mrow></math
     >.
