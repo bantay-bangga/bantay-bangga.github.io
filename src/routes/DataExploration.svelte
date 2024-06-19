a5c0b5952b9cd4615f52fdc3a169ac2bb0386c7d
Anjellyrika <98273014+Anjellyrika@users.noreply.github.com>
2024-06-19 12:14:33 +0800

    refactor: use proper filename convention

--- src/routes/DataExploration.svelte
+++ src/routes/DataExploration.svelte
@@ -1,9 +1,10 @@
 <script>
     import PlotlyScatter from '$lib/components/PlotlyScatter.svelte';
-    import weekly_bed_data from '$lib/plots/data/weekly_bed_occupancy.json';
-    import weekly_equip_data from '$lib/plots/data/weekly_equip.json';
-    import weekly_injury_data from '$lib/plots/data/weekly_injuries.json';
-    import weekly_staff_data from '$lib/plots/data/weekly_staff.json';
+    import WeeklyBedData from '$lib/plots/data/weekly-bed-occupancy.json';
+    import WeeklyEquipData from '$lib/plots/data/weekly-equip.json';
+    import WeeklyInjuryData from '$lib/plots/data/weekly-injuries.json';
+    import WeeklyStaffData from '$lib/plots/data/weekly-staff.json';
+    import TotalBedsVsMinor from '$lib/plots/data/totalbeds-vs-minor.json';
 </script>
 
 <h3>Weekly Trends</h3>
@@ -14,7 +15,7 @@
     computed using the number of incidents of a particular severity over the total number of incidents. The data is then
     aggregated by week and by month. We show the weekly trend in injury numbers:
 </p>
-<PlotlyScatter data={weekly_injury_data} />
+<PlotlyScatter data={WeeklyInjuryData} />
 
 <h4>Bed Occupancy Percentage by Week</h4>
 <p>
@@ -22,26 +23,22 @@
     and vacant). The data is then aggregated by week and by month by getting the mean occupancy percentage for the
     week/month. We show the weekly trend in bed occupancy percentage:
 </p>
-<PlotlyScatter data={weekly_bed_data} />
+<PlotlyScatter data={WeeklyBedData} />
 
 <h4>Average Medical Staff Availability by Week</h4>
 <p>
     The medical staff data is aggregated as a sum per day, and by the mean per week and per month. We show the weekly
     trend:
 </p>
-<PlotlyScatter data={weekly_staff_data} />
+<PlotlyScatter data={WeeklyStaffData} />
 
 <h4>Total Medical Equipment by Week</h4>
 <p>The medical equipment data is aggregated as a sum per day, week and month. We show the weekly trend:</p>
-<PlotlyScatter data={weekly_equip_data} />
+<PlotlyScatter data={WeeklyEquipData} />
 
 <h3>Research Question 1: To what extent does hospital capacity affect the fatality rate of road incidents?</h3>
 <h4>Bed Occupancy vs. Minor Injuries</h4>
-<enhanced:img
-    src="$lib/plots/totalbeds_vs_minor.png"
-    alt="A scatter plot of total occupied hospital bed percentage vs minor road incident injuries"
-    class="mx-auto block max-w-2xl"
-/>
+<PlotlyScatter data={TotalBedsVsMinor} showLegend={false} />
 <p>
     First, the total number of road crash victims with <b>minor</b> injuries is compared to the total bed occupancy.
     Eyeballing the plot shows a slight linear relationship between the two variables with a correlation coefficient
